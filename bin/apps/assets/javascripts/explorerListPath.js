952dfa2b0727e4e130246b3b5edbb67add8935d4
= <hugo.chilemme@gmail.com>
2023-06-13 17:25:42 +0200

    Optimize performance & correcting bug

--- bin/apps/assets/javascripts/explorerListPath.js
+++ bin/apps/assets/javascripts/explorerListPath.js
@@ -8,10 +8,10 @@ const setDisplayedPath = (path) => {
     
     let folders = path.split('/').slice(1);
     current_path = folders;
-
+    
     if (path === "/") return;
-
-
+    
+    
     let teleportPath = [];
     for (let i = 0; i < folders.length; i++) {
         const e = folders[i];
@@ -29,9 +29,9 @@ const setDisplayedPath = (path) => {
 
 function formatBytes(bytes) {
     if (bytes === 0) {
-      return '0 B';
+        return '0 B';
     }
-  
+    
     const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
     const k = 1024;
     const i = Math.floor(Math.log(bytes) / Math.log(k));
@@ -40,63 +40,67 @@ function formatBytes(bytes) {
   }
 
   
-const CreateInterfaceItem = (file) => {
-    var row = document.createElement('tr');
-
-    row.addEventListener('dblclick', () => {
-        row.style.backgroundColor = "rgba(46, 161, 219, 0.25)";
-        if (file.type === "d") {
-            if (file.name === "..") {
-                current_path.pop();
-            } else {
-                current_path.push(file.name);
+  const CreateInterfaceItem = (file) => {
+      if (file.name !== ".." && file.name[0] == ".") {
+          return;
+        }
+        
+        var row = document.createElement('tr');
+        
+        row.addEventListener('dblclick', () => {
+            row.style.backgroundColor = "rgba(46, 161, 219, 0.25)";
+            if (file.type === "d") {
+                if (file.name === "..") {
+                    current_path.pop();
+                } else {
+                    current_path.push(file.name);
             }
             loadPath('/' + current_path.join('/'));
         }
     })
-
-
+    
+    
     var iconCell = document.createElement('td');
     var icon = document.createElement('i');
     icon.className = file.type === 'd' ? 'bx bx-folder' : 'bx bx-file-blank'; 
     iconCell.appendChild(icon);
     row.appendChild(iconCell);
-
+    
     var fileNameCell = document.createElement('td');
     fileNameCell.textContent = file.name;
     row.appendChild(fileNameCell);
-
+    
     var sizeCell = document.createElement('td');
     if (file.size) {
         sizeCell.textContent = formatBytes(file.size);
     }
     row.appendChild(sizeCell);
-
+    
     var lastModifiedCell = document.createElement('td');
-
+    
     if (file.modifyTime) {
         const modifyTime = new Date(file.modifyTime);
         lastModifiedCell.textContent = modifyTime.toLocaleDateString() + ' ' + modifyTime.toLocaleTimeString() || "";
     }
     row.appendChild(lastModifiedCell);
-
+    
     var permissionCell = document.createElement('td');
     if (file.longname) {
         permissionCell.textContent = file.longname.split(' ')[0];
     }
     row.appendChild(permissionCell);
-
+    
     var ownerCell = document.createElement('td');
     if (file.username) {
         ownerCell.textContent = file.username;
     }
     row.appendChild(ownerCell);
-
+    
     var tableBody = document.querySelector('#explorer-files'); // Sélectionnez votre élément tbody existant
     tableBody.appendChild(row);
-
+    
     return tableBody;
-
+    
 }
 
 const displayFoldersAndFiles = async (files) => {
@@ -110,9 +114,11 @@ const displayFoldersAndFiles = async (files) => {
 global.loadPath = async path => {
     const working_e = document.querySelector('#explorer-files');
     working_e.innerHTML = "";
-
+    
     const repository = await ipcRenderer.invoke('explorerList', path);
-
+    
+    localStorage.setItem('latest_path', path);
+    
     setDisplayedPath(repository.path);
     displayFoldersAndFiles(repository.result);
 }
@@ -123,5 +129,6 @@ const handleLoad = () => {
     loadPath(path);
 }
 
+document.querySelector('#refresh-button').addEventListener('click', handleLoad);
 document.querySelector('[page-name="explorer"]').addEventListener("onshow", handleLoad);
 document.querySelector('.explorer .header .route').addEventListener('click', () => loadPath('/'));
