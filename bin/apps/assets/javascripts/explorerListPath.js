2ace59c61d3b37dd1b02d19d21e7f1349876a0ba
= <hugo.chilemme@gmail.com>
2023-06-13 18:35:10 +0200

    Adding sort functionnality

--- bin/apps/assets/javascripts/explorerListPath.js
+++ bin/apps/assets/javascripts/explorerListPath.js
@@ -50,19 +50,23 @@ function formatBytes(bytes) {
         row.addEventListener('dblclick', () => {
             row.style.backgroundColor = "rgba(46, 161, 219, 0.25)";
             if (file.type === "d") {
-                if (file.name === "..") {
-                    current_path.pop();
-                } else {
-                    current_path.push(file.name);
+                if (file.name === "..") current_path.pop();
+                else current_path.push(file.name);
+                return loadPath('/' + current_path.join('/'));
             }
-            loadPath('/' + current_path.join('/'));
-        }
-    })
+            console.log('fichier');
+
+        })
     
     
     var iconCell = document.createElement('td');
     var icon = document.createElement('i');
-    icon.className = file.type === 'd' ? 'bx bx-folder' : 'bx bx-file-blank'; 
+    icon.className = 'bx bx-file-blank'; 
+    if (file.type === "d") {
+        icon.className = 'bx bx-folder';
+    } else if(file.type === "l") {
+        icon.className = 'bx bx-link-external';
+    }
     iconCell.appendChild(icon);
     row.appendChild(iconCell);
     
@@ -71,7 +75,7 @@ function formatBytes(bytes) {
     row.appendChild(fileNameCell);
     
     var sizeCell = document.createElement('td');
-    if (file.size) {
+    if (file.size || file.size === 0) {
         sizeCell.textContent = formatBytes(file.size);
     }
     row.appendChild(sizeCell);
@@ -114,21 +118,52 @@ const displayFoldersAndFiles = async (files, path) => {
 global.loadPath = async path => {
     const working_e = document.querySelector('#explorer-files');
     working_e.innerHTML = "";
+    let repository = await ipcRenderer.invoke('explorerList', path);
+    const sort_by = JSON.parse(localStorage.getItem('sort_by')) || { sort: 'desc', name: 'type' };
     
-    const repository = await ipcRenderer.invoke('explorerList', path);
-    
-    localStorage.setItem('latest_path', path);
+    console.log(repository.result)
+    repository.result.sort((a, b) => {
+        if (a[sort_by.name] < b[sort_by.name]) {
+            return -1;
+        }
+        if (a[sort_by.name] > b[sort_by.name]) {
+            return 1;
+        }
+        return 0;
+    });
 
+    if (sort_by.sort === 'desc') {
+        repository.result.reverse();
+    }
+    localStorage.setItem('latest_path', path);
+    
     setDisplayedPath(repository.path);
     displayFoldersAndFiles(repository.result, repository.path);
 }
 
 const handleLoad = () => {
     const path = localStorage.getItem('latest_path');
-
+    
     loadPath(path);
+}
+
+
+const handleOrder = (e) => {
+    let sort_by = JSON.parse(localStorage.getItem('sort_by')) || { sort: 'desc', name: 'type' };
+    const oName = e.target.getAttribute('order-name');
+    if (!oName) return;
+    
+    if (sort_by.name === oName) {
+        sort_by.sort = sort_by.sort === "asc" ? 'desc' : 'asc';
+    } else {
+        sort_by.sort = 'asc';
+    }
+    sort_by.name = oName;
+    localStorage.setItem('sort_by', JSON.stringify(sort_by));
+    handleLoad();
 }
 
 document.querySelector('#refresh-button').addEventListener('click', handleLoad);
 document.querySelector('[page-name="explorer"]').addEventListener("onshow", handleLoad);
 document.querySelector('.explorer .header .route').addEventListener('click', () => loadPath('/'));
+document.querySelectorAll('.explorer thead th').forEach(e => e.addEventListener('click', handleOrder));
