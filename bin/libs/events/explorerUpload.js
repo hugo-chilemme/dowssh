b1e1b071590c9de85b1ad8dd3089ff4a6ca64d73
= <hugo.chilemme@gmail.com>
2023-06-13 17:26:31 +0200

    Adding upload functionnality

--- bin/libs/events/explorerUpload.js
+++ bin/libs/events/explorerUpload.js
@@ -1,9 +1,16 @@
 const { ipcMain, BrowserWindow } = require('electron');
 const SessionStorage = require('../SessionStorage');
+const fs = require('fs');
 
 ipcMain.handle('explorerUpload', async (event, localPath, remotePath) => {
+    console.log(localPath, remotePath);
    try {
-        await active_session.updir(localPath, remotePath);
+        const stats = fs.statSync(localPath);
+        if (stats.isDirectory()) {
+            await active_session.updir(localPath, remotePath);
+        } else {
+            await active_session.upfile(localPath, remotePath);
+        }
         return {ok: true, result: remotePath};
    } catch(e) {
         return {ok: false, message: e.message}
